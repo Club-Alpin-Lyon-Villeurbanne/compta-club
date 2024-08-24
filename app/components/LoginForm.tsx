c55c1f45898606ab3de92c552eccaa488dc79cdd
Kévin Liagre <kevin.liagre@gmail.com>
2024-08-24 17:01:02 +0200

    Fix error after login

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import React, {useState} from 'react';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
   const [username, setUsername] = useState<String>("");
@@ -12,73 +12,82 @@ export default function LoginForm() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    signIn('credentials', {
+    await signIn("credentials", {
       redirect: false,
       username: username,
       password: password,
-    })
-    .then(r => {
-      if (r?.ok) {
-        router.push('/note-de-frais');
-      } else {
-        throw new Error('Une erreur est survenue', {cause: r});
-      }
-    })
-    .catch(e => console.log(e))
-    ;
+    });
+    // .then(r => {
+    //   if (r?.ok) {
+    //     router.push('/note-de-frais');
+    //   } else {
+    //     throw new Error('Une erreur est survenue', {cause: r});
+    //   }
+    // })
+    // .catch(e => console.log(e))
+    // ;
   };
 
-    return (
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSubmit} className="space-y-6">
-                <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Adresse email
-                    </label>
-                    <div className="mt-2">
-                        <input
-                            type="text"
-                            placeholder="exemple@exemple.fr"
-                            value={username as string}
-                            onChange={(e) => setUsername(e.target.value)}
-                            autoComplete={username as string}
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                            Mot de passe
-                        </label>
-                        <div className="text-sm">
-                            {/* TODO: Créer la route "forgot-password" */}
-                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                Mot de passe oublié ?
-                            </a>
-                        </div>
-                    </div>
-                    <div className="mt-2">
-                        <input
-                            type="password"
-                            placeholder="Mot de passe"
-                            value={password as string}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Se connecter
-                    </button>
-                </div>
-            </form>
+  return (
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Adresse email
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              placeholder="exemple@exemple.fr"
+              value={username as string}
+              onChange={(e) => setUsername(e.target.value)}
+              autoComplete={username as string}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-    );
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Mot de passe
+            </label>
+            <div className="text-sm">
+              {/* TODO: Créer la route "forgot-password" */}
+              <a
+                href="#"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Mot de passe oublié ?
+              </a>
+            </div>
+          </div>
+          <div className="mt-2">
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              value={password as string}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Se connecter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
