19ffccec19de04d8c181018c57ba59d0cae441af
Kévin Liagre <kevin.liagre@gmail.com>
2024-08-24 17:19:22 +0200

    Fix reject action

--- app/enums/ExpenseStatus.ts
+++ app/enums/ExpenseStatus.ts
@@ -1,32 +1,32 @@
 export enum ExpenseStatus {
-    DRAFT = 'draft',
-    SUBMITTED = 'submitted',
-    PENDING = 'pending',
-    VALIDATED = 'validated',
-    REJECT = 'reject',
-    APPROVED = 'approved',
-    VALIDATE = 'validate',
+  DRAFT = "draft",
+  SUBMITTED = "submitted",
+  PENDING = "pending",
+  VALIDATED = "validated",
+  REJECT = "rejected",
+  APPROVED = "approved",
+  VALIDATE = "validate",
 }
 
 export const getExpenseStatusTranslation = (status: ExpenseStatus): string => {
-    switch (status) {
-        case ExpenseStatus.DRAFT:
-            return 'Brouillon';
-        case ExpenseStatus.SUBMITTED:
-            return 'Soumis';
-        case ExpenseStatus.PENDING:
-            return 'En attente';
-        case ExpenseStatus.VALIDATED:
-            return 'Validé';
-        case ExpenseStatus.VALIDATE:
-            return 'Valider';
-        case ExpenseStatus.REJECT:
-            return 'Rejeté';
-        case ExpenseStatus.APPROVED:
-            return 'Approuvé';
-        default:
-            return 'Inconnu';
-    }
+  switch (status) {
+    case ExpenseStatus.DRAFT:
+      return "Brouillon";
+    case ExpenseStatus.SUBMITTED:
+      return "Soumis";
+    case ExpenseStatus.PENDING:
+      return "En attente";
+    case ExpenseStatus.VALIDATED:
+      return "Validé";
+    case ExpenseStatus.VALIDATE:
+      return "Valider";
+    case ExpenseStatus.REJECT:
+      return "Rejeté";
+    case ExpenseStatus.APPROVED:
+      return "Approuvé";
+    default:
+      return "Inconnu";
+  }
 };
 
 export default ExpenseStatus;
