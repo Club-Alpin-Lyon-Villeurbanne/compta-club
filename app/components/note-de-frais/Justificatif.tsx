7c915deef9a2d86abd3495b9900edff705111b89
Nicolas Ritouet <nicolas@ritouet.com>
2024-12-19 20:21:15 +0100

    Fix: fileurl of attachements

--- app/components/note-de-frais/Justificatif.tsx
+++ app/components/note-de-frais/Justificatif.tsx
@@ -4,7 +4,7 @@ export const Justificatif = ({ fileUrl }: { fileUrl: string | undefined }) => {
     return (
         <>
             {fileUrl ? (
-                <a href={process.env.NEXT_PUBLIC_WEBSITE_BASE_URL + fileUrl} target="_blank"
+                <a href={fileUrl} target="_blank"
                     rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">
                     <FaFileAlt className="inline-block w-5 h-5" />
                 </a>
